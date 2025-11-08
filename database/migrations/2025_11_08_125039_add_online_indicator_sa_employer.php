f83dda9ea2c2be7a291476c21d592677f26e98e0
Opaw <pagawpawjemoyacerenado@gmail.com>
2025-11-08 14:51:59 +0800

    New file added - gising kana khad gawa kana

---
+++ database/migrations/2025_11_08_125039_add_online_indicator_sa_employer.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employer_account', function (Blueprint $table) {
+            $table->boolean('is_online')->default(false);
+            $table->timestamp('last_seen')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employer_account', function (Blueprint $table) {
+            $table->dropColumn('is_online');
+            $table->dropColumn('last_seen');
+        });
+    }
+};
